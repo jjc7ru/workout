527c23cdb4df9a94d0155de967417ded7f01ade0
Jason Choi <jjc7ru@gmail.com>
2022-07-20 14:11:05 -0400

    Initial commit

--- workout.js
+++ workout.js
@@ -2,16 +2,37 @@ const express = require('express');
 const morgan = require('morgan');
 const session = require("express-session");
 const store = require("connect-loki");
+const flash = require("express-flash");
 
 const app = express();
 const port = 3000;
 const host = 'localhost';
+const LokiStore = store(session);
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(morgan("common"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000, // 31 days in millseconds
+    path: "/",
+    secure: false,
+  },
+  name: "workout-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "to be replaced later with config file",
+  store: new LokiStore({}),
+}));
+
+app.use(flash());
+
 app.get("/", (req, res) => {
-  res.render("layout");
+  res.render("lists");
 });
 
 // Error handler
